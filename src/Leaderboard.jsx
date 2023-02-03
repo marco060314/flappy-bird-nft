bec456d4ed1479e0bd01aaea6c3d1447af65f8e4
Marco Wang <marco060314@gmail.com>
2023-02-02 21:02:35 -0800

    Update Leaderboard.jsx

--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -14,16 +14,33 @@ import { useAccount } from 'wagmi'
 import { useEffect } from 'react'
 
 const Leaderboard = ({}) => {
+  const [leaderboard, setLeaderboard] = React.useState([])
   useEffect(() => {
     const leaderboardRef = query(ref(db, 'leaderboard'), orderByChild('score'))
     onValue(leaderboardRef, (snapshot) => {
       const data = snapshot.val()
       console.log(data)
+      setLeaderboard(Object.values(data))
     })
-  })
+  }, [])
+  console.log(leaderboard)
   return (
     <div class="">
       <h1 class="text-6xl font-semibold">Leaderboard</h1>
+      {leaderboard.map((player, index) => (
+        <div class="flex-row flex">
+          <p>{index + 1}</p>
+          <p>
+            {player.address.slice(0, 4)}....{player.address.slice(-4)}
+          </p>
+          <p>{player.score}</p>
+          <img
+            class="h-10 w-10 object-contain"
+            src={player.nfturl}
+            alt="nfts"
+          />
+        </div>
+      ))}
     </div>
   )
 }
